86215cee24fcb6e6437a6e46a79bf7923b23aac6
Javier López <fjavierlh@gmail.com>
2025-11-10 16:52:06 +0100

    refactor: create constant for base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,9 @@ import { defineConfig, devices } from "@playwright/test";
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+const baseURL = process.env.BASE_URL || "http://localhost:5173/";
+
 export default defineConfig({
   testDir: "./src/tests/e2e",
   /* Run tests in files in parallel */
@@ -26,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:5173/",
+    baseURL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
@@ -50,7 +53,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    url: "http://localhost:5173/",
+    url: baseURL,
     reuseExistingServer: !!process.env.CI,
   },
 });
