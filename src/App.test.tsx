710831e324360a8364ee8ee96fe37e99475c378e
Javier López <fjavierlh@gmail.com>
2025-11-10 12:32:58 +0100

    feat: integrate use case in ui

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { PropsWithChildren } from "react";
 
 describe("App", () => {
   it("should render", () => {
-    render(<App />);
-    expect(screen.getByText(/vite \+ react/i)).toBeInTheDocument();
+    renderWithProviders(<App />);
+    expect(screen.getByText(/loading.../i)).toBeInTheDocument();
   });
 });
+
+export const QueryClientWrapper = ({ children }: PropsWithChildren) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+function renderWithProviders(ui: React.ReactElement) {
+  return render(<QueryClientWrapper>{ui}</QueryClientWrapper>);
+}
