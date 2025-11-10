882ad50caec3ee796227a105eb805189cdc6a6bd
Javier López <fjavierlh@gmail.com>
2025-11-10 14:48:14 +0100

    fix: modify url of web server and update test

--- src/tests/e2e/example.spec.ts
+++ src/tests/e2e/example.spec.ts
@@ -1,10 +1,20 @@
 import { test, expect } from "@playwright/test";
+import type { UserDTO } from "../../core/user/infrastructure/user.dto";
 
-test.describe("Smoke test", () => {
-  test("should load the base URL without errors", async ({ page }) => {
+test.describe("Application", () => {
+  test("should load the base URL and display user names", async ({ page }) => {
     await page.goto("/");
-    await expect(page).toHaveTitle(/.+/);
-    await expect(page.locator("body")).toBeVisible();
-    await expect(page.locator("text=vite + react")).toBeVisible();
+    const request = await page.waitForResponse(
+      (req) =>
+        req.url().includes("https://jsonplaceholder.typicode.com/users") &&
+        req.status() === 200
+    );
+
+    const data: UserDTO[] = await request.json();
+    const userNames = data.map((user) => user.name);
+
+    for (const name of userNames) {
+      await expect(page.getByText(name)).toBeVisible();
+    }
   });
 });
