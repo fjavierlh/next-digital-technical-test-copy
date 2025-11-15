21563a08f373895a70515de72e7a4f569484f796
Javier López <fjavierlh@gmail.com>
2025-11-15 17:45:16 +0100

    feat: create query todo list use case

---
+++ src/core/todos/application/query-todo-list-by-user-id.use-case.test.ts
@@ -0,0 +1,28 @@
+import type { Mocked } from "vitest";
+import { todoListMock } from "../domain/todo.model.mock";
+import type { TodoRepository } from "../domain/todo.repository";
+import { QueryTodoListByUserIdUseCase } from "./query-todo-list-by-user-id.use-case";
+
+describe("QueryTodoListByUserIdUseCase", () => {
+  let useCase: QueryTodoListByUserIdUseCase;
+  let todoRepository: Mocked<TodoRepository>;
+
+  beforeEach(() => {
+    todoRepository = {
+      byId: vi.fn(),
+      byUserId: vi.fn().mockResolvedValue(todoListMock),
+      create: vi.fn(),
+      delete: vi.fn(),
+    };
+
+    useCase = new QueryTodoListByUserIdUseCase(todoRepository);
+  });
+
+  it("should return all todos for a given user", async () => {
+    const userId = "1";
+    const result = await useCase.execute(userId);
+
+    expect(todoRepository.byUserId).toHaveBeenCalledWith(userId);
+    expect(result).toEqual(todoListMock);
+  });
+});
---
+++ src/core/todos/application/query-todo-list-by-user-id.use-case.ts
@@ -0,0 +1,15 @@
+import { UserId } from "../../user/domain/user-id.vo";
+import type { TodoRepository } from "../domain/todo.repository";
+
+export class QueryTodoListByUserIdUseCase {
+  private readonly todoRepository: TodoRepository;
+
+  constructor(todoRepository: TodoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
+  async execute(userId: string) {
+    const userIdVO = UserId.create(userId);
+    return this.todoRepository.byUserId(userIdVO);
+  }
+}
