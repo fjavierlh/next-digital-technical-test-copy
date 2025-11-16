baf681ed1f4c75037fcac4c3245dc2f471062f21
Javier López <fjavierlh@gmail.com>
2025-11-16 22:52:05 +0100

    feat: add missing query invalidation

--- src/core/todos/ui/hooks/useCreateTodoUseCase.ts
+++ src/core/todos/ui/hooks/useCreateTodoUseCase.ts
@@ -1,8 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { CreateTodoUseCase } from "../../application/command/create-todo.use-case";
 import { TodosRestRepository } from "../../infrastructure/todos-rest-repository";
 
 export const useCreateTodoUseCase = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: async (params: {
       userId: string;
@@ -13,5 +15,8 @@ export const useCreateTodoUseCase = () => {
         title: params.title,
         completed: params.completed,
       }),
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({ queryKey: ["todos", variables.userId] });
+    },
   });
 };
